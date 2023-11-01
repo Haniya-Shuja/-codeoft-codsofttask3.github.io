0a667a53116edfeac620759012005894951ef3c7
Haniya-Shuja <116838089+Haniya-Shuja@users.noreply.github.com>
2023-11-01 19:20:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// let string = "";
+// let button = document.querySelectorAll('button');
+// Array.from('buttons').forEach(('button')=>{
+// button.addEventListener('click', (e)=>{ 
+//     if(e.target .innerHtml== '='){
+//         string = eval (string)
+//         document.querySelector('input').value string;
+//     }
+//     console.log(e.target);
+//     string = string + e.target.innerHtml;
+//     document.querySelector('input').value string;
+//     })
+// })
+
+
+let string = "";
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML === '=') {
+      try {
+        string = eval(string);
+        document.querySelector('input').value = string;
+      } catch (error) {
+        document.querySelector('input').value = 'Error';
+      }
+      
+    } else if (e.target.innerHTML === 'C') {
+      string = '';
+      document.querySelector('input').value = '';
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  });
+});
